17a9a26801729f07d80fd8a4bc702ed828f683a5
Will <wrbrown@aggies.ncat.edu>
2019-11-12 19:46:57 -0500

    Initial Commit

---
+++ Assets/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPercent = navMeshAgent.velocity.magnitude / navMeshAgent.speed;
+        animator.SetFloat("speedPercent", speedPercent, .1f, Time.deltaTime);
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    RaycastHit hit;
+    [SerializeField] Vector3 cameraOffset = new Vector3(0f, 7f, 7f);
+    
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Camera.main.transform.position = transform.position + cameraOffset;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
