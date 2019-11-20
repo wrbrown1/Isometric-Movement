75865940521874740784c0cf4bf240321da334a6
Will <wrbrown@aggies.ncat.edu>
2019-11-19 20:58:00 -0500

    Added some movement/attacking animations

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -7,18 +7,46 @@ public class PlayerAnimator : MonoBehaviour
 {
     Animator animator;
     NavMeshAgent navMeshAgent;
+    bool moving = false;
+    float speed = 0f;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        animator = GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float speed = navMeshAgent.velocity.magnitude / navMeshAgent.speed;
+        AnimateMovement();
+        CheckIfMoving();
+        //AttackWhenNotMoving();
+    }
+
+    //private void AttackWhenNotMoving()
+    //{
+    //    if (Input.GetMouseButtonDown(1) && !moving)
+    //    {
+    //        animator.SetTrigger("attack");
+    //        print("Attack");
+    //    }
+    //}
+
+    private void AnimateMovement()
+    {
+        speed = navMeshAgent.velocity.magnitude / navMeshAgent.speed;
         animator.SetFloat("speed", speed, .1f, Time.deltaTime);
-        
+    }
+
+    private void CheckIfMoving()
+    {
+        if (speed == 0)
+        {
+            moving = false;
+        }
+        else
+        {
+            moving = true;
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,22 +9,66 @@ public class PlayerMovement : MonoBehaviour
     NavMeshAgent agent;
     RaycastHit hit;
     [SerializeField] Vector3 cameraOffset = new Vector3(0f, 7f, 7f);
-    
+    bool canMove = true;
+    Animator animator;
+    float speed;
+    bool attacking = false;
+
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        speed = agent.velocity.magnitude;
         Camera.main.transform.position = transform.position + cameraOffset;
-        if (Input.GetMouseButton(0))
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetMouseButton(0) && canMove)
         {
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            {
-                agent.destination = hit.point;
-            }
+            MoveToClick();
         }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Stop();
+            Attack();
+        }
+    }
+
+    private void Stop()
+    {
+        agent.destination = transform.position;
+    }
+
+    private void MoveToClick()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            agent.destination = hit.point;
+        }
+    }
+
+    private void Attack()
+    {
+        print("attacking");
+        animator.SetTrigger("attack");
+        canMove = false;
+        Invoke("AllowToMove", .5f);
+    }
+
+    void AllowToMove()
+    {
+        canMove = true;
+    }
+
+    private bool JustFinishedMoving()
+    {
+        return true;
     }
 }
